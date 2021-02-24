e339b6976d7e29e7973ba53704b4c2b1a575f8bd
JagdeshU <42053910+JagdeshU@users.noreply.github.com>
2021-02-24 14:05:30 +0530

    some changes

--- src/io/jagdesh/aaLambdaExpression/afLambda2.java
+++ src/io/jagdesh/aaLambdaExpression/afLambda2.java
@@ -1,60 +1,12 @@
 package io.jagdesh.aaLambdaExpression;
 
+import io.jagdesh.common.Person;
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class afLambda2 {
-
-    public static class Person{
-
-        private String firstName;
-        private String lastName;
-        private int age;
-
-        public Person() {
-        }
-
-        public Person(String firstName, String lastName, int age) {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.age = age;
-        }
-
-        public String getFirstName() {
-            return firstName;
-        }
-
-        public void setFirstName(String firstName) {
-            this.firstName = firstName;
-        }
-
-        public String getLastName() {
-            return lastName;
-        }
-
-        public void setLastName(String lastName) {
-            this.lastName = lastName;
-        }
-
-        public int getAge() {
-            return age;
-        }
-
-        public void setAge(int age) {
-            this.age = age;
-        }
-
-        @Override
-        public String toString() {
-            return "Person{" +
-                    "firstName='" + firstName + '\'' +
-                    ", lastName='" + lastName + '\'' +
-                    ", age=" + age +
-                    '}';
-        }
-
-    }
 
     //  an interface
     interface Condition{
