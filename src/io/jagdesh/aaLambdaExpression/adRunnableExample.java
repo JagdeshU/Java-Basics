4cc99ed4ad213d9bf654139ba1ec71ee60aa73f8
JagdeshU <42053910+JagdeshU@users.noreply.github.com>
2021-02-24 13:41:37 +0530

    some changes

---
+++ src/io/jagdesh/aaLambdaExpression/adRunnableExample.java
@@ -0,0 +1,23 @@
+package io.jagdesh.aaLambdaExpression;
+
+public class adRunnableExample {
+
+    public static void main(String[] args) {
+
+        Thread myThread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                System.out.println("Printed inside runnable");
+            }
+
+        });
+
+        myThread.run();
+
+        Thread myLambdaThread = new Thread(() -> System.out.println("Printed inside Lambda Runnable"));
+        myLambdaThread.run();
+
+    }
+
+}
