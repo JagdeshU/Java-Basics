b1e9e0339f3dbfd11917d877ae4327ca699a6b74
JagdeshU <42053910+JagdeshU@users.noreply.github.com>
2021-02-24 12:25:22 +0530

    some changes

--- src/io/jagdesh/aaLambdaExpression/aaLambda1.java
+++ src/io/jagdesh/aaLambdaExpression/aaLambda1.java
@@ -1,11 +1,37 @@
 package io.jagdesh.aaLambdaExpression;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class aaLambda1 {
 
     public static void main(String[] args) {
 
+        List<String> nameList = new ArrayList<>();
 
+        nameList.add("Tom");
+        nameList.add("Hank");
+        nameList.add("Sean");
+        nameList.add("Richard");
+
+        //  using Lambda Expression
+        nameList.forEach((name) -> System.out.println(name));
+        System.out.println("-------------------");
+
+        //  without Lambda Expression - function call
+        printName(nameList);
 
     }
+
+    //  Function Declaration for implementing function without Lambda Expression
+    private static void printName(List<String> nameList) {
+
+        for (int i=0; i<nameList.size(); i++){
+            System.out.println(nameList.get(i));
+        }
+        System.out.println("-------------------");
+
+    }
+
 
 }
